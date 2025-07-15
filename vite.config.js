f22064ea6a35edc8819fd02851de8285ee9283fe
abhissh11 <dev.abhishekkr@gmail.com>
2025-07-15 13:40:05 +0530

    fix: import path module in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
